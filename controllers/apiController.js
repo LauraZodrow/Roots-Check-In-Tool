1feaf55bc13a7f6dc5a26a29538c3581d3409400
Laura <laura.zodrow@gmail.com>
2015-04-14 20:28:49 -0600

    casing changes again

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,6 +1,6 @@
 var https = require('https');
 var Scan = require('../models/Scan');
-var User = require('../models/User');
+var User = require('../models/user');
 var _ = require('lodash');
 var moment = require('moment');
 var async = require('async');
